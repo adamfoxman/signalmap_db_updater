e6b0441a84bd8aeb5abcdef3a759a29b7aac9ac5
adamfoxman <adam.foxman@outlook.com>
2021-11-25 18:43:02 +0100

    Changed formula in antennafilegenerator.py because previous one was bad

--- dbupdater/antennafilegenerator.py
+++ dbupdater/antennafilegenerator.py
@@ -1,5 +1,5 @@
 from textwrap import wrap
-from math import ceil
+from math import ceil, sqrt
 
 
 def get_antenna_file(path: str,
@@ -11,20 +11,14 @@ def get_antenna_file(path: str,
                      pattern_v: str):
     if path[-1] != "/":
         path += "/"
-    antenna_filename = f"{path}{country}_{type}_{id}.ant"
-    # try:
-    #     antenna_file = open(antenna_filename, "w")
-    # except OSError:
-    #     print(f"Can't open file {antenna_filename}")
-    #     return
+    antenna_filename = f"{path}{country}_{type}_{id}.az"
     with open(antenna_filename, "w") as antenna_file:
         antenna_file = open(antenna_filename, "w+")
         pattern = []
 
         if pattern_h == "" and pattern_v == "" and antenna_direction != "":
+            # to be implemented
             return None
-
-        print(pattern_h)
 
         h_max_value, pattern_h = split_string_to_list(pattern_h)
         v_max_value, pattern_v = split_string_to_list(pattern_v)
@@ -32,25 +26,32 @@ def get_antenna_file(path: str,
         if pattern_h is not None and pattern_v is not None and len(pattern_h) == len(pattern_v):
             for i in range(len(pattern_h)):
                 pattern.append(max(pattern_h[i], pattern_v[i]))
-                max_value = max(h_max_value, v_max_value)
+                max_value_dbw = max(h_max_value, v_max_value)
         elif pattern_h is not None and pattern_v is None:
             pattern = pattern_h
-            max_value = h_max_value
+            max_value_dbw = h_max_value
         elif pattern_h is None and pattern_v is not None:
             pattern = pattern_v
-            max_value = v_max_value
+            max_value_dbw = v_max_value
+        else:
+            return None
+
+        max_value = sqrt((10 ** (max_value_dbw / 10)) * 75)
 
         antenna_file.write("0\n")
 
         for i in range(0, 360):
             left_side_value = float(pattern[ceil((i + 1) / 10) - 1 % 36])
             right_side_value = float(pattern[ceil((i + 1) / 10) % 36])
-            value = float(right_side_value * (float(i) % 10.0) + left_side_value * (10.0 - (float(i) % 10.0))) / max_value / 10.0
+            value_dbw = float(
+                right_side_value * (float(i) % 10.0) + left_side_value * (10.0 - (float(i) % 10.0))) / 10.0
+            value = sqrt((10 ** (value_dbw / 10)) * 75) / max_value
             value = round(value, 6)
             antenna_file.write(str(i) + '\t' + str(value) + "\n")
 
         antenna_file.close()
         return antenna_filename
+
 
 def split_string_to_list(pattern):
     if pattern == "":
@@ -62,4 +63,3 @@ def split_string_to_list(pattern):
         max_value = max(pattern)
         print(pattern)
         return max_value, pattern
-
